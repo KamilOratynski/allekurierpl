10243db5783bc1be3c34ea59261ea638e3a06ad8
KamilOratynski <kamil.oratynski@gmail.com>
2023-03-19 12:38:10 +0100

    Cleared after disabling Gherkin syntax

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,25 +1,11 @@
 import { defineConfig } from "cypress";
-import { addCucumberPreprocessorPlugin } from "@badeball/cypress-cucumber-preprocessor";
-import browserify from "@badeball/cypress-cucumber-preprocessor/browserify";
-
-async function setupNodeEvents(
-  on: Cypress.PluginEvents,
-  config: Cypress.PluginConfigOptions
-): Promise<Cypress.PluginConfigOptions> {
-  await addCucumberPreprocessorPlugin(on, config);
-  on(
-    "file:preprocessor",
-    browserify(config, {
-      typescript: require.resolve("typescript")
-    })
-  );
-  return config;
-}
 
 export default defineConfig({
   e2e: {
     baseUrl: "https://nowy.allekurier.pl/",
-    setupNodeEvents
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
   },
   env: {
     hideXHRInCommandLog: true,
